85c618fabf05171ee0a0276c38b8f3e0fbf48279
Natalie Johnstone <nhj@illinois.edu>
2025-07-09 12:05:39 -0500

    updating rich text paragraph

---
+++ js/paragraph-rt.js
@@ -0,0 +1,5 @@
+jQuery(function($) {
+  $('ilw-columns .paragraph--type--rt ilw-content[width="auto"]').removeAttr('width');
+  $('ilw-columns .paragraph--type--rt ilw-columns[width="auto"]').removeAttr('width');
+  $('ilw-columns .paragraph--type--contact ilw-columns[width="auto"]').removeAttr('width');
+});
