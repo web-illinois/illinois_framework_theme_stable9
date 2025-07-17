49514b8ec44a93bb9e9ad778eeae97a845863e7f
Natalie Johnstone <nhj@illinois.edu>
2025-07-17 11:05:06 -0500

    padding tweak on list paragraph

--- js/paragraph-list-content.js
+++ js/paragraph-list-content.js
@@ -1,4 +1,4 @@
 jQuery(function($) {
   $('ilw-columns .paragraph--type--list ilw-content[width="auto"][theme="gray"]').removeAttr('width').attr('padding', '15px 20px 30px 20px');
-  $('ilw-columns .paragraph--type--list ilw-content[width="auto"]:not([theme="gray"])').removeAttr('width').attr('padding', '15px 0 30px 0');
+  $('ilw-columns .paragraph--type--list ilw-content[width="auto"]:not([theme="gray"])').removeAttr('width').attr('padding', '30px 0');
 });
