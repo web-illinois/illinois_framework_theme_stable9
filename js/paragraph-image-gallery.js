e05978bcedd97049aa15fdc48f9c9b7da15fbdee
Natalie Johnstone <nhj@illinois.edu>
2025-09-08 16:56:44 -0500

    paragraph image gallery to toolkit 3

---
+++ js/paragraph-image-gallery.js
@@ -0,0 +1,4 @@
+jQuery(function($) {
+  $('ilw-columns .paragraph--type--image-gallery ilw-content[width="auto"]').removeAttr('width');
+  $('ilw-columns .paragraph--type--image-gallery ilw-grid[width="auto"]').removeAttr('width');
+});
