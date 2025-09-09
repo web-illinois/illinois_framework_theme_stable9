ce125be8d7cf86f1265005472406c19e8c016be2
Natalie Johnstone <nhj@illinois.edu>
2025-09-09 16:35:05 -0500

    paragraph vertical tab to toolkit3

---
+++ js/paragraph-vertical-tab.js
@@ -0,0 +1,4 @@
+jQuery(function($) {
+  $('ilw-columns .paragraph--type--vertical-tab ilw-content[width="auto"]').removeAttr('width').attr('padding', '0 0 0 30px');
+  $('ilw-columns .paragraph--type--vertical-tab ilw-tabs[width="auto"]').removeAttr('width');
+});
